9654db49bc0bb4deae76cba16f7a550de9d0aa10
Qingyang Li <congel@gmail.com>
2019-01-26 16:31:39 -0800

    fix pass

--- optimize_something/optimization.py
+++ optimize_something/optimization.py
@@ -186,3 +186,4 @@ if __name__ == "__main__":
     # This code WILL NOT be called by the auto grader 			  		 			     			  	   		   	  			  	
     # Do not assume that it will be called 			  		 			     			  	   		   	  			  	
     #test_code()
+    pass
