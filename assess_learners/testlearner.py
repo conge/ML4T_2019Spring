dfc1b1e72a35d873a3510fc52d881645d089784b
Qingyang Li <congel@gmail.com>
2019-02-11 19:12:45 +0800

    created Report. coded experiment 3 to test processing speed

--- assess_learners/testlearner.py
+++ assess_learners/testlearner.py
@@ -29,6 +29,7 @@ import DTLearner as DT
 import RTLearner as RT
 import BagLearner as bl
 import sys
+import time
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -190,7 +191,7 @@ if __name__=="__main__":
     # plot the data
 
     #x = range(1, max_leaf_size + 1)
-    plt.figure(2)
+    plt.figure(3)
     plt.plot(x, rmse_in_sample, label="in-sample", linewidth=2.0)
     plt.plot(x, rmse_out_sample, label="out-of-sample", linewidth=2.0)
     plt.xlabel("Leaf Size")
@@ -233,7 +234,7 @@ if __name__=="__main__":
     # plot the data
 
     #x = range(1, max_leaf_size + 1)
-    plt.figure(2)
+    plt.figure(4)
     plt.plot(x, rmse_in_sample, label="in-sample", linewidth=2.0)
     plt.plot(x, rmse_out_sample, label="out-of-sample", linewidth=2.0)
     plt.xlabel("Leaf Size")
@@ -248,6 +249,83 @@ if __name__=="__main__":
     # in figure 1, smaller leaf sizes are more likely to overfit
     # Find and print the index of the first leaf_size that does not overfit
     print "EXP2-3b: DT with bagging stopped overfit when leaf size is larger than ", np.argmin(rmse_out_sample ) + 1
+
+    # =========== EXPERiMENT 3 ===================
+
+    """ Test training time and querying time of DT and RT learners
+    """
+    portions = np.array(range(1,10))/10.0
+    leaf_size = 6
+
+    train_time_DT = []
+    query_time_DT = []
+    train_time_RT = []
+    query_time_DT = []
+
+
+    for p in portions:
+        # compute how much of the data is training and testing
+        train_rows = int(p * data.shape[0])
+        test_rows = data.shape[0] - train_rows
+
+        # separate out training and testing data
+        trainX = data[:train_rows,0:-1]
+        trainY = data[:train_rows,-1]
+        testX = data[train_rows:,0:-1]
+        testY = data[train_rows:,-1]
+
+        # dt learner
+        learner = DT.DTLearner(leaf_size, verbose = False) # create a dt learner
+
+        start = time.time()
+        learner.addEvidence(trainX, trainY) # train it
+        train_finish = time.time()
+
+        # evaluate in sample
+        predY = learner.query(trainX) # get the predictions
+        query_finish = time.time()
+
+        train_time_DT.append(train_finish - start)
+        query_time_DT.append(query_finish - train_finish)
+
+        # Rt learner
+        learner = RT.RTLearner(leaf_size, verbose = False) # create a dt learner
+        start = time.time()
+        learner.addEvidence(trainX, trainY) # train it
+        train_finish = time.time()
+        # evaluate in sample
+        predY = learner.query(trainX) # get the predictions
+        query_finish = time.time()
+
+        train_time_R.append( train_finish - start)
+        query_time_RT.append(query_finish - train_finish)
+
+    query_time_RT = query_time_RT[::-1]
+    query_time_DT = query_time_DT[::-1]
+
+    plt.figure(5)
+    x = portions
+    plt.plot(x, train_time_DT, label="Decision Tree", linewidth=2.0)
+    plt.plot(x, train_time_RT, label="Random Tree", linewidth=2.0)
+    plt.xlabel("Dataset size (proportion)")
+    plt.ylabel("Speed")
+    plt.legend(loc="lower right")
+    plt.title("Compute time to train")
+    plt.savefig('Exp3_fig4a.png')
+    plt.close()
+
+    plt.figure(6)
+    x = portions
+    plt.plot(x, query_time_DT, label="Decision Tree", linewidth=2.0)
+    plt.plot(x, query_time_RT, label="Random Tree", linewidth=2.0)
+    plt.xlabel("Dataset size (proportion)")
+    plt.ylabel("Speed")
+    plt.legend(loc="lower right")
+    plt.title("Compute time to query")
+    plt.savefig('Exp3_fig4b.png')
+    plt.close()
+
+
 
 
 
