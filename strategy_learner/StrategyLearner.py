c3f705bdf45e87ff6e762e6317a1f2b5b2655847
Qingyang Li <congel@gmail.com>
2019-04-21 03:19:46 -0700

    line 77]

--- strategy_learner/StrategyLearner.py
+++ strategy_learner/StrategyLearner.py
@@ -74,6 +74,7 @@ class StrategyLearner(object):
         :param ret: return rate of the next day
         :return: updated holdings and reward
         """
+        print("77 holdings, action,ret = ",holdings, ", ",action,", ",ret)
         rewards = 0.0
         if holdings == -1000: # shorting position
             if action <= 1: # Action = { Do nothing or 0 =Short},
