1bc0aacda781da6413aa188ce4af331b00163f2a
Qingyang Li <congel@gmail.com>
2019-04-21 17:15:57 -0700

    initial reward = -1

--- strategy_learner/StrategyLearner.py
+++ strategy_learner/StrategyLearner.py
@@ -189,7 +189,7 @@ class StrategyLearner(object):
             #df_trades = df_prices.copy()
             #df_trades[:] = 0.0
 
-            reward = 0.0
+            reward = -1
             #print("SL 171: PSR.shape[0] = ",PSR.shape[0],"; daily_returns.shape[0] = ",daily_returns.shape[0])
 
             # Cycle through dates
