d8b1c457b24eb94a46166e4a6d02c6eae14df043
Qingyang Li <congel@gmail.com>
2019-04-21 18:24:16 -0700

    MS

--- strategy_learner/ManualStrategy.py
+++ strategy_learner/ManualStrategy.py
@@ -58,9 +58,9 @@ class ManualStrategy(object):
 
 
 def generate_orders(df_trades,symbol):
-    print "MS 61 df_trades[-1] = ",df_trades[-1], "................"
+    print "MS 61 df_trades[-1] = ",df_trades.iloc[-1], "................"
     df_orders = df_trades.copy()
-    df_orders[-1] = 1
+    df_orders.iloc[-1] = 1
     print "MS 64: df_trades.index[-1]", df_trades.index[-1]
 
     df_orders = df_orders.loc[(df_orders.Trades != 0)]
